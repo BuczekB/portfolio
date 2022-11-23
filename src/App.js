afbdc5f0198ee3de7edfcfc6df0e20e3782280d5
Jakuba Buczyński <72788108+BuczekB@users.noreply.github.com>
2022-11-23 18:14:39 +0100

    add prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Button, Link } from 'react-scroll';
-import { HashRouter as Router, Switch, Route} from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { motion, AnimatePresence } from "framer-motion"
+import { Link } from 'react-scroll';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './App.css';
 import Cube from './Cube';
-import Card from './Card';
 import WorkBox from './WorkBox';
 import WebTest from './WebTest';
 import ArrowUp from './ArrowUp';
@@ -13,173 +11,206 @@ import WeatherApp from './WeatherApp';
 import BJGame from './BJGame';
 import Website from './Website';
 
-import heroImage from "./img/profilowe3.png";
+import heroImage from './img/profilowe3.png';
 
 function App() {
-
-  
-  
-
   return (
     <Router>
-      <div className="App" >
+      <div className="App">
+        <div id="backToUp"></div>
 
-        <div id='backToUp'></div>
-
-        <div className='main' >
+        <div className="main">
           <nav>
-            <div className='logo' id='logo'>
-              <h2 >Jakub</h2>
+            <div className="logo" id="logo">
+              <h2>Jakub</h2>
             </div>
-            <div className='navigation'>
-
-              <span >
-                <Link activeClass="active" className='navigationParagraph' to="about" spy={true} smooth={true} offset={-100} duration={500}>
+            <div className="navigation">
+              <span>
+                <Link
+                  activeClass="active"
+                  className="navigationParagraph"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                >
                   About
                 </Link>
               </span>
               <span>
-                <Link activeClass="active" className='navigationParagraph' to="works" spy={true} smooth={true}  duration={500}>
+                <Link
+                  activeClass="active"
+                  className="navigationParagraph"
+                  to="works"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
                   Works
                 </Link>
               </span>
-
-
-
             </div>
           </nav>
-          <div className='hero'>
-
-            <div className='left'>
-            
-              <div className='heroImage'>
-                <img  className='imageHero' width='730px' height='940px' alt='profilowe3.png' src={heroImage}></img>
-              </div> 
-              <div className='circle'></div>
-            </div >
-            <div className='right'>
-              <div className='mainInfo'>
-                <motion.h1 
-                initial={{x:'100vw', opacity:0}}
-                animate={{x:0, opacity:1}}
-                
-                >Witam jestem 
-                <motion.span 
-                initial={{x:'100vw', opacity:0}}
-                animate={{x:0, opacity:1}}
-                className='orangeSpan'> Jakub! </motion.span>
+          <div className="hero">
+            <div className="left">
+              <div className="heroImage">
+                <img
+                  className="imageHero"
+                  width="730px"
+                  height="940px"
+                  alt="profilowe3.png"
+                  src={heroImage}
+                ></img>
+              </div>
+              <div className="circle"></div>
+            </div>
+            <div className="right">
+              <div className="mainInfo">
+                <motion.h1
+                  initial={{ x: '100vw', opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                >
+                  Witam jestem
+                  <motion.span
+                    initial={{ x: '100vw', opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    className="orangeSpan"
+                  >
+                    {' '}
+                    Jakub!{' '}
+                  </motion.span>
                 </motion.h1>
-                <motion.h3 
-                initial={{x:'100vw', opacity:0}}
-                animate={{x:0, opacity:1}}
-                transition={{delay: 0.3}}
-                className='juniorH3'>Junior Frontend Developer</motion.h3>
-               
-                
+                <motion.h3
+                  initial={{ x: '100vw', opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                  className="juniorH3"
+                >
+                  Junior Frontend Developer
+                </motion.h3>
               </div>
               <div>
-               
-                  <Link activeClass="active" to="more" spy={true} smooth={true} offset={-100} duration={500}>
-                  <motion.button 
-                  initial={{x:'100vw', opacity:0}}
-                  animate={{x:0, opacity:1}}
-                  transition={{delay: 0.65}}
-                  className='moreButton' type='button'>More</motion.button>
+                <Link
+                  activeClass="active"
+                  to="more"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                >
+                  <motion.button
+                    initial={{ x: '100vw', opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ delay: 0.65 }}
+                    className="moreButton"
+                    type="button"
+                  >
+                    More
+                  </motion.button>
                 </Link>
-                
               </div>
-              <div className='cubeBox'>
-
+              <div className="cubeBox">
                 <Cube bg={'js'}></Cube>
                 <Cube bg={'html'}></Cube>
                 <Cube bg={'css'}></Cube>
                 <Cube bg={'react'}></Cube>
-
-
-              </div >
+              </div>
             </div>
-          </div >
-        </div >
-        <div className='about' id='about'>
-          <div className="aboutMe" id='more'>
+          </div>
+        </div>
+        <div className="about" id="about">
+          <div className="aboutMe" id="more">
             <h3>
-              <span className='orangeSpan'> About </span>
-              Me</h3>
+              <span className="orangeSpan"> About </span>
+              Me
+            </h3>
             <motion.p
-            initial={{opacity:0}}
-            whileInView={{opacity:1}}
-            transition={{duration:1.2, delay:0.2}}>
-                  Cześć! Jestem Jakub i mój aktualny poziom w zakresie umiejętności IT to Junior Fronted/JavaScript Developer. Właśnie znajdujesz się na stronie na której przedstawiam moje  najciekawsze projekty. Sam/a możesz dzięki temu określić poziom moich aktualnych umiejętności.
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.2, delay: 0.2 }}
+            >
+              Cześć! Jestem Jakub i mój aktualny poziom w zakresie umiejętności
+              IT to Junior Fronted/JavaScript Developer. Właśnie znajdujesz się
+              na stronie na której przedstawiam moje najciekawsze projekty.
+              Sam/a możesz dzięki temu określić poziom moich aktualnych
+              umiejętności.
             </motion.p>
-           <motion.p
-            initial={{opacity:0}}
-            whileInView={{opacity:1}}
-            transition={{duration:1.2, delay:0.2}}>
-            Przygodę z programowaniem zacząłem 3 lata temu i od pierwszego momentu pochłonęło mnie to całkowicie. W pracy tej najbardziej lubię to że na bieżąco mogę uczyć się nowych rzeczy, dzięki temu że technologia wciąż idzie do przodu. Ponadto nadwyraz polubiłem to uczucie  satysfakcji gdy po długiej batali z kodem w końcu udaje się rozwiązać ten nurtujący problem.  Od tego momentu ciągle pogłębiam swoją wiedzę w tym zakresie. Swoją naukę zacząłem od c++ oraz pythona, jednak najwięcej swojego czasu poświęciłem na JavaScript i frameWork z nim zwiazany- React. Poznając oczywiście po drodze wiele narzędzi pomocnych w tworzeniu stron i aplikacji webowych. Fakt, nie jestem studentem informatyki ani kierowników pobocznych. Od początku ambitnie podchodzę do zadania i  uczę się sam. Wyłącznie rzeczy które uważam za istotne i które rokują potencjalnie na przyszłe lata. Uważam że od papierka ważniejsze są posiadane umiejętności i motywacja do działania.</motion.p>
-
+            <motion.p
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.2, delay: 0.2 }}
+            >
+              Przygodę z programowaniem zacząłem 3 lata temu i od pierwszego
+              momentu pochłonęło mnie to całkowicie. W pracy tej najbardziej
+              lubię to że na bieżąco mogę uczyć się nowych rzeczy, dzięki temu
+              że technologia wciąż idzie do przodu. Ponadto nadwyraz polubiłem
+              to uczucie satysfakcji gdy po długiej batali z kodem w końcu udaje
+              się rozwiązać ten nurtujący problem. Od tego momentu ciągle
+              pogłębiam swoją wiedzę w tym zakresie. Swoją naukę zacząłem od c++
+              oraz pythona, jednak najwięcej swojego czasu poświęciłem na
+              JavaScript i frameWork z nim zwiazany- React. Poznając oczywiście
+              po drodze wiele narzędzi pomocnych w tworzeniu stron i aplikacji
+              webowych. Fakt, nie jestem studentem informatyki ani kierowników
+              pobocznych. Od początku ambitnie podchodzę do zadania i uczę się
+              sam. Wyłącznie rzeczy które uważam za istotne i które rokują
+              potencjalnie na przyszłe lata. Uważam że od papierka ważniejsze są
+              posiadane umiejętności i motywacja do działania.
+            </motion.p>
           </div>
           <div className="laptop">
-            <motion.div 
-            initial={{opacity:0}}
-            whileInView={{opacity:1}}
-            transition={{duration:1.2, delay:0.2}}
-            className='stack'>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.2, delay: 0.2 }}
+              className="stack"
+            >
               <h3>Stack</h3>
-              <div className='stackList'>
-              <ul>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Html</li>
-                <li>CSS</li>
-              </ul>
-              <ul>
-                <li>GIT</li>
-                <li>API</li>
-                <li>RWD</li>
-                <li>SASS</li>
-              </ul>
+              <div className="stackList">
+                <ul>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>Html</li>
+                  <li>CSS</li>
+                </ul>
+                <ul>
+                  <li>GIT</li>
+                  <li>API</li>
+                  <li>RWD</li>
+                  <li>SASS</li>
+                </ul>
               </div>
             </motion.div>
-            <motion.div 
-             initial={{opacity:0}}
-             whileInView={{opacity:1}}
-             transition={{duration:1.2, delay:0.15}}
-            className='contact'>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.2, delay: 0.15 }}
+              className="contact"
+            >
               <h3>Contact</h3>
-              
-              <ul >
+
+              <ul>
                 <li>E-mail: buczynskijakub10@gmail.com</li>
                 <li>GitHub: https://github.com/BuczekB</li>
                 <li>Phone Number: +48 735 157 016</li>
               </ul>
-              
             </motion.div>
           </div>
-         </div>
-            <div className='works' id='works'>
+        </div>
+        <div className="works" id="works">
+          <Switch>
+            <Route path="/" exact component={WorkBox} />
+            <Route path="/BJ Game" component={BJGame} />
+            <Route path="/Website" component={Website} />
+            <Route path="/Weather App" component={WeatherApp} />
+            <Route path="/WebTest" component={WebTest} />
+          </Switch>
+        </div>
 
-
-            
-              <Switch>
-                <Route path='/' exact component={WorkBox} />
-                <Route path='/BJ Game' component={BJGame} />
-                <Route path='/Website' component={Website} />
-                <Route path='/Weather App' component={WeatherApp} />
-                <Route path='/WebTest' component={WebTest} />
-              </Switch>
-            
-
-
-
-
-
-            </div>
-
-        <ArrowUp/>
-        <div className='mobileVersion'>
-        <span>Mobile Version</span>
-        <span>coming soon</span>
-
+        <ArrowUp />
+        <div className="mobileVersion">
+          <span>Mobile Version</span>
+          <span>coming soon</span>
         </div>
       </div>
     </Router>
--- src/ArrowUp.js
+++ src/ArrowUp.js
@@ -1,63 +1,51 @@
 import React, { useEffect, useState } from 'react';
 
-
-import { Link } from 'react-scroll'
+import { Link } from 'react-scroll';
 import './arrowUp.css';
 
-
-
-
-
-
 function ArrowUp() {
+  const [flag, setFlag] = useState(0);
 
+  useEffect(() => {
+    window.addEventListener('scroll', arrow);
+  }, []);
 
+  const arrow = () => {
+    let heightLevel = window.scrollY;
+    const showLevel = 200;
 
-    const [flag, setFlag] = useState(0)
+    console.log(heightLevel);
 
+    if (heightLevel > showLevel) setFlag(1);
+    else setFlag(0);
+  };
 
-    useEffect(() => {
-
-        window.addEventListener('scroll', arrow);
-
-
-    }, [])
-
-    const arrow = () => {
-
-
-
-        let heightLevel = window.scrollY;
-        const showLevel = 200;
-
-        console.log(heightLevel);
-
-        if (heightLevel > showLevel) setFlag(1)
-        else setFlag(0)
-
-    }
-
-
-
-    if (flag) {
-        return (
-
-
-            <div className='moveToUp'>
-                <Link activeClass="active" className='navigationParagraph' to="backToUp" spy={true} smooth={true} offset={-50} duration={500}>
-                    <span className='arrow'>
-                        <svg className='arroww' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z" /></svg>
-                    </span>
-                </Link>
-            </div>
-
-        );
-    }
-    return
-
+  if (flag) {
+    return (
+      <div className="moveToUp">
+        <Link
+          activeClass="active"
+          className="navigationParagraph"
+          to="backToUp"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          <span className="arrow">
+            <svg
+              className="arroww"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z" />
+            </svg>
+          </span>
+        </Link>
+      </div>
+    );
+  }
+  return;
 }
 
-
-export default ArrowUp
-
-
+export default ArrowUp;
